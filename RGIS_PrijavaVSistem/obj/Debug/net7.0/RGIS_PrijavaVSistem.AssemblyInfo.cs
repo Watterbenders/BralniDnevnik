bedf66a7f27023ba511cbd2e5bb5418fee35e5bc
Watterbenders <kristjan.gersak@student.um.si>
2025-12-03 11:08:51 +0100

    x

--- RGIS_PrijavaVSistem/obj/Debug/net7.0/RGIS_PrijavaVSistem.AssemblyInfo.cs
+++ RGIS_PrijavaVSistem/obj/Debug/net7.0/RGIS_PrijavaVSistem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RGIS_PrijavaVSistem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc94971faf66f38a94ab9cbd4cedafd61116b506")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b2c2646e1d7d37f9b03b831f79f879b1ffbaf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RGIS_PrijavaVSistem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RGIS_PrijavaVSistem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
