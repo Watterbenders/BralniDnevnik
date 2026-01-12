872cc5a6a37374e4f7b772a1f7759c19829361c7
OnlyMushy <maks.ferkulj88@gmail.com>
2026-01-12 16:25:28 +0100

    fsdc

--- RGIS_PrijavaVSistem/obj/Debug/net7.0/RGIS_PrijavaVSistem.AssemblyInfo.cs
+++ RGIS_PrijavaVSistem/obj/Debug/net7.0/RGIS_PrijavaVSistem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RGIS_PrijavaVSistem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b717b8cc47e5834c4fae95a6c473763fd00566")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+949256483ae3e84c01809e7f70659f16d28670c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("RGIS_PrijavaVSistem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RGIS_PrijavaVSistem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
